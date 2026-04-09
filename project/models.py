3a86550df5c8378cb66d5c099a7eac2ae3af133d
LDonn32 <lauradonnelly32@gmail.com>
2026-04-09 12:11:16 +0100

    basic models drafted, basic HTML landing page and requirement doc updated

---
+++ project/models.py
@@ -0,0 +1,63 @@
+import flask 
+import flask_sqlalchemy
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    date_of_birth = db.Column(db.Date)
+    qualifications = db.relationship('Qualification', backref='student')
+    exam_results = db.relationship('ExamResult', backref='student')
+
+class Qualification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+
+class ExamResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(100))
+    grade = db.Column(db.String(5))
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+
+
+db = SQLAlchemy()
+
+# Student model with additional fields and relationships
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    date_of_birth = db.Column(db.Date)
+    qualifications = db.relationship('Qualification', backref='student')
+    exam_results = db.relationship('ExamResult', backref='student')
+
+class Qualification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+
+class ExamResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(100))
+    grade = db.Column(db.String(5))
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+
+
+
+# Teacher model with relationship to Class model
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    classes = db.relationship('Class', backref='teacher')
+
+class Class(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    class_name = db.Column(db.String(50))
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'))
